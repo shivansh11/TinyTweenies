93d06c4b821c18738e27cb740c87016cfc7a786d
Shivansh Agrawal <shivansh11@outlook.com>
2018-08-22 12:48:49 +0530

    Made obstacles as prefabs. Applied ObstaclePool script (misleading name since we have not used the actual object pooling). Everything works fine so far.

--- Assets/Scripts/ImpactManager.cs
+++ Assets/Scripts/ImpactManager.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class ImpactManager : MonoBehaviour {
     public GameObject TT1, TT2;
     public Animator anim1, anim2;
-    // Use this for initialization
+
+    public float lifetime = 5f;
+    private float timeSinceLastSpawned;
+
     void Start () {
+        TT1 = GameObject.Find("TT1");
+        TT2 = GameObject.Find("TT2");
         anim1 = TT1.GetComponent<Animator>();
         anim2 = TT2.GetComponent<Animator>();
     }
 	
-	// Update is called once per frame
+
 	void Update () {
-		
+        timeSinceLastSpawned += Time.deltaTime;
+        if (timeSinceLastSpawned >= lifetime)
+            Destroy(gameObject);
 	}
 
     private void OnCollisionEnter2D(Collision2D collision){
---
+++ Assets/Scripts/ObstaclePool.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstaclePool : MonoBehaviour {
+
+    public GameObject WindmillPrefab;
+    public GameObject SpikePrefab;
+    public GameObject BombPrefab;
+    public GameObject UFOPrefab;
+    public float spawnRate = 2f;
+
+    private GameObject Windmill;
+    private GameObject Spike;
+    private GameObject Bomb;
+    private GameObject UFO;
+    private Vector2 obstaclePoolPosition = new Vector2(-20f, -20f);
+    private float timeSinceLastSpawned;
+    private float spawnXPosition = 10f;
+    private int obstacleIndex1;
+    private int obstacleIndex2;
+
+    void Start () {
+       // Windmill = (GameObject)Instantiate(WindmillPrefab, obstaclePoolPosition, WindmillPrefab.transform.rotation);
+       // Spike = (GameObject)Instantiate(SpikePrefab, obstaclePoolPosition, SpikePrefab.transform.rotation);
+       // Bomb = (GameObject)Instantiate(BombPrefab, obstaclePoolPosition, Quaternion.identity);
+       // UFO = (GameObject)Instantiate(UFOPrefab, obstaclePoolPosition, Quaternion.identity);
+    }
+	
+	void Update () {
+        timeSinceLastSpawned += Time.deltaTime;
+
+        if (timeSinceLastSpawned >= spawnRate) {
+            timeSinceLastSpawned = 0f;
+            obstacleIndex1 = Random.Range(0,3);
+            obstacleIndex2 = Random.Range(-2, 1);
+
+            if (obstacleIndex1 == 0 || obstacleIndex2 == -obstacleIndex1 || obstacleIndex2 == 0) {
+                SpawnObstacles(obstacleIndex1, obstacleIndex2);
+            }
+        }
+	}
+
+    public void SpawnObstacles(int obstacleIndex1, int obstacleIndex2) {
+        if (obstacleIndex1 == 1)
+            Bomb = (GameObject)Instantiate(BombPrefab, new Vector2(spawnXPosition, 0.55f), Quaternion.identity);
+            //Bomb.transform.position = new Vector2(spawnXPosition, 0.55f);
+        else if(obstacleIndex1 == 2)
+            UFO = (GameObject)Instantiate(UFOPrefab, new Vector2(spawnXPosition, 2.65f), Quaternion.identity);
+            //UFO.transform.position = new Vector2(spawnXPosition, 2.65f);
+
+        if (obstacleIndex2 == -1)
+            Spike = (GameObject)Instantiate(SpikePrefab, new Vector2(spawnXPosition, -0.5f), SpikePrefab.transform.rotation);
+            //Spike.transform.position = new Vector2(spawnXPosition, -0.5f);
+        else if (obstacleIndex2 == -2)
+            Windmill = (GameObject)Instantiate(WindmillPrefab, new Vector2(spawnXPosition, -1.42f), WindmillPrefab.transform.rotation);
+            //Windmill.transform.position = new Vector2(spawnXPosition, -1.42f);
+    }
+}
