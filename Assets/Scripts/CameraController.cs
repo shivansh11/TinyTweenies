32cc693efdb50a1afb478926a638e3b4ecc0005f
Shivansh Agrawal <shivansh11@outlook.com>
2018-08-25 14:01:32 +0530

    Added SlideShake ability to camera on slide

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject TT1, TT2;
+    public Animator anim1, anim2, anim;
+
+    private int isSlideShake = 0;
+
+    void Start () {
+        anim1 = TT1.GetComponent<Animator>();
+        anim2 = TT2.GetComponent<Animator>();
+        anim = gameObject.GetComponent<Animator>();
+    }
+	
+
+	void Update () {
+        if (isSlideShake == 0 && anim1.GetAnimatorTransitionInfo(0).IsName("TT1Jump -> TT1Slide")) {
+            isSlideShake = 1;
+            StartCoroutine(SlideShake());
+        }
+	}
+
+    IEnumerator SlideShake() {
+        yield return new WaitForSeconds(0.1f);
+        anim.SetTrigger("isSlideShake");
+        yield return new WaitForSeconds(0.2f);
+        isSlideShake = 0;
+    }
+}
