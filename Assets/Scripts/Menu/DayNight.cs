6c53103fbbb2eb23663b054cf46f487f1388e110
Shivansh Agrawal <shivansh11@outlook.com>
2018-09-16 13:43:23 +0530

    Added very basic GradientSky (Shader) and DayNight code

---
+++ Assets/Scripts/Menu/DayNight.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNight : MonoBehaviour {
+
+    //Always change the color properties through Inspector
+    public Color Morning;
+    public Color Day;
+    public Color Evening;
+    public Color Night;
+
+    private int indra = 0;
+    private float t = 0;
+
+    void Start () {
+        gameObject.GetComponent<Renderer>().material.SetColor("_Color", Night);
+	}
+
+	void Update () {
+        if (indra == 0) {
+            gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.Lerp(Morning, Day, t));
+            gameObject.GetComponent<Renderer>().material.SetColor("_Color1", Color.Lerp(Evening, Night, t));
+        } else if (indra == 1) {
+            gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.Lerp(Day, Evening, t));
+            gameObject.GetComponent<Renderer>().material.SetColor("_Color1", Color.Lerp(Night, Morning, t));
+        } else if (indra == 2) {
+            gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.Lerp(Evening, Night, t));
+            gameObject.GetComponent<Renderer>().material.SetColor("_Color1", Color.Lerp(Morning, Day, t));
+        } else {
+            gameObject.GetComponent<Renderer>().material.SetColor("_Color", Color.Lerp(Night, Morning, t));
+            gameObject.GetComponent<Renderer>().material.SetColor("_Color1", Color.Lerp(Day, Evening, t));
+        }
+
+        t += Time.deltaTime / 20;
+        if (t >= 1.0f) {
+            t = 0f;
+            indra = (indra + 1) % 4;
+        }
+    }
+
+}
